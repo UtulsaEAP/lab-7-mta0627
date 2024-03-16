2ee683ef2c1a126ab905f4091ee8f4f36c215759
Mason Anderson <mta0627@utulsa.edu>
2024-03-15 23:42:23 -0500

    Wrote code for problem 2

--- Problem 2/wordInRange.py
+++ Problem 2/wordInRange.py
@@ -1,5 +1,32 @@
+'''
+Name: Mason Anderson
+Time: 3/15/24 11:41 PM
+'''
+
+
 def wordInRange():
     #Type your code here
-    return
+        
+    with open(input(), 'r') as file:
+ 
+        line = file.readline()
+        
+        range_min = input()
+
+        range_max = input()
+
+        while line:
+
+            if range_min < line < range_max:
+
+                print(f'{line.strip()} - in range')
+            
+            else:
+
+                print(f'{line.strip()} - not in range')
+
+            line = file.readline()
+
+    
 if __name__ == '__main__':
     wordInRange()
